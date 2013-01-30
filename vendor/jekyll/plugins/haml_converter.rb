bd61e4aab93696c1ad25985f384f4142f1fa92ac
benastan <bennyjbergstein@gmail.com>
2013-01-30 13:34:29 -0800

    Added plugins

---
+++ vendor/jekyll/plugins/haml_converter.rb
@@ -0,0 +1,22 @@
+# _plugins/haml_converter.rb
+
+module Jekyll
+   require 'haml'
+  class HamlConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /haml/i
+    end
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def convert(content)
+      engine = Haml::Engine.new(content)
+      content = engine.render
+    end
+  end
+end
---
+++ vendor/jekyll/plugins/jekyll_assets.rb
@@ -0,0 +1,40 @@
+require 'jekyll-assets'
+require 'jekyll-assets/compass'
+
+module Jekyll
+  module AssetsPlugin
+    class Renderer
+      def render_asset_path type
+        unless @site.static_files.include? asset
+          @site.static_files << AssetFile.new(@site, asset)
+        end
+
+        if type
+          if @site.assets_config.respond_to?(prefix_key = "#{type}_prefix")
+            prefix = @site.assets_config.send(prefix_key)
+          end
+        end
+
+        path = []
+        [ @site.assets_config.baseurl, prefix ].each do |segment|
+          path << segment unless segment.nil? || segment == ''
+        end
+        path << asset.digest_path
+
+        return path.join '/'
+      end
+
+      def render_javascript
+        @path << '.js' if File.extname(@path).empty?
+
+        JAVASCRIPT % render_asset_path(:js)
+      end
+
+      def render_stylesheet
+        @path << '.css' if File.extname(@path).empty?
+
+        STYLESHEET % render_asset_path(:css)
+      end
+    end
+  end
+end
